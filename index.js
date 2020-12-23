8773949ae0a178c717bdbf128b010d636203a3ba
Mihai Stoica <41210245+MichaelVonWolfen@users.noreply.github.com>
2020-12-23 13:14:25 +0200

    Added image manipulation for the jpg and png files

--- index.js
+++ index.js
@@ -2,47 +2,36 @@
 // exports.handler = async function (event) {
 //   //here you will add your code
 // };
-const path = require("path");
+const Jimp = require("jimp");
 const fs = require("fs");
-const sharp = require("sharp");
-// let images_folder = path.join(__dirname, "images");
-// fs.readdirSync(images_folder).forEach(async (image) => {
-//   let image_path = path.join(images_folder, image);
-//   let optimized = path.join(__dirname, "optimized", image);
 
-//   let sharp_image = sharp(image_path);
-//   let image_metadata = await sharp_image.metadata();
-//   let width = image_metadata.width;
-//   let height = image_metadata.height;
-//   console.log(`Width: ${width} \t Height: ${height}`);
-
-//   let min = Math.min(height, width);
-//   let max = Math.max(height, width);
-
-//   let choosen_size = min;
-//   if (choosen_size === width) {
-//     //choosen_size = width
-//     sharp_image
-//       .resize({
-//         width: 500,
-//       })
-//       .toFile(optimized)
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   } else {
-//     //choosen_size = height
-//     sharp_image
-//       .resize({
-//         height: 500,
-//       })
-//       .toFile(optimized)
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   }
-// });
-fs.readdirSync("./optimized").forEach(async (image) => {
-  let meta = await sharp(path.join(__dirname, "optimized", "image")).metadata();
-  console.log(`${image} width: ${meta.width} \t height: ${meta.height}`);
+var images_folder = __dirname + "/images";
+//for each image in the images folder
+fs.readdir(images_folder, (err, images) => {
+  images.forEach((image) => {
+    let img_path = images_folder + "/" + image;
+    //read the image
+    Jimp.read(img_path).then((img) => {
+      /*
+      / Find the minimum size between the width and height
+      */
+      let h = img.bitmap.height;
+      let w = img.bitmap.width;
+      let min = Math.min(h, w);
+      console.log(`${image} \t ${h} \t ${w}`);
+      /*
+      upscale or downdcale the width or height at 500 px
+      */
+      if (min === h) {
+        img.resize(Jimp.AUTO, 500);
+      } else {
+        img.resize(500, Jimp.AUTO);
+      }
+      img.quality(40).write("./optimized/" + image);
+    });
+  });
 });
+// (async function () {
+//   image.resize(500, Jimp.AUTO);
+//   image.write("./optimized/image1.jpg");
+// })();
